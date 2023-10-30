000cfd2253f3bf83b67addc574c853400985ad7e
sergio-villegas <sergio1012012.1@gmail.com>
2023-10-29 21:13:12 -0600

    Funcionalidad de contenedores, navbar y boton sin accion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,156 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'kanban-fire';
+
+  containers = [
+    {
+      elementName: 'HEDS user manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'Tool for Huawei device configuration',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    },
+    {
+      elementName: 'VPN Cisco AnyConnect user manual (chinese version)',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'Ticket generator manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'How to use OWS-104 platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'How to create a brand new VM using OSA platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    },
+    {
+      elementName: 'HEDS user manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'Tool for Huawei device configuration',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    },
+    {
+      elementName: 'VPN Cisco AnyConnect user manual (chinese version)',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'Ticket generator manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'How to use OWS-104 platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'How to create a brand new VM using OSA platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    },
+    {
+      elementName: 'HEDS user manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'Tool for Huawei device configuration',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    },
+    {
+      elementName: 'VPN Cisco AnyConnect user manual (chinese version)',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'Ticket generator manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'How to use OWS-104 platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'How to create a brand new VM using OSA platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    },
+    {
+      elementName: 'HEDS user manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'Tool for Huawei device configuration',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    },
+    {
+      elementName: 'VPN Cisco AnyConnect user manual (chinese version)',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'Ticket generator manual',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'FO'
+    },
+    {
+      elementName: 'How to use OWS-104 platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IT&SOC'
+    },
+    {
+      elementName: 'How to create a brand new VM using OSA platform basic user guide',
+      user: 's84323766',
+      date: '10/27/2023',
+      workArea: 'IP'
+    }
+    ];
+  
+    
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
