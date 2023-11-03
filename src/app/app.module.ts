712e6ee921e43d31778f7166bb60160e0efb1377
sergio-villegas <sergio1012012.1@gmail.com>
2023-11-02 20:41:42 -0600

    checkpoint 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,12 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
